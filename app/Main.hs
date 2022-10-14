3c87e1c90314611ef4207014592767417af81099
Dhananjay Balan <mail@dbalan.in>
2022-10-14 10:10:35 +0200

    Inital test.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Network.Pushover
+import Network.Pushover.Request
+import Network.Pushover.Token
+import qualified Data.Text as T
+import Data.Maybe
+
+testRequest = Request
+  { requestToken      = makeTokenOrError "foo"
+  , requestUserKey    = makeTokenOrError "bar"
+  , requestMessage    = text "Backup of database \"example\" finished in 16 minutes."
+  , devices           = ["iphone12"]
+  , title             = Just "Backup finished - SQL1"
+  , url               = Nothing
+  , priority          = Nothing
+  , timestamp         = Nothing
+  , notificationSound = Nothing
+  }
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  r <- sendRequest testRequest
+
+  putStrLn $ show $ status r
