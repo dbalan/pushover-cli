43acc1d6c34cd55d5367a09d15ff967f60532a97
Dhananjay Balan <mail@dbalan.in>
2022-10-17 09:18:18 +0200

    pushover cli

--- app/Main.hs
+++ app/Main.hs
@@ -5,22 +5,67 @@ import Network.Pushover
 import Network.Pushover.Request
 import Network.Pushover.Token
 import qualified Data.Text as T
+import Data.Text (Text)
+import System.Environment
 import Data.Maybe
+import UnliftIO.Exception
+import Options.Applicative
 
-testRequest = Request
-  { requestToken      = makeTokenOrError "foo"
-  , requestUserKey    = makeTokenOrError "bar"
-  , requestMessage    = text "Backup of database \"example\" finished in 16 minutes."
-  , devices           = ["iphone12"]
-  , title             = Just "Backup finished - SQL1"
-  , url               = Nothing
-  , priority          = Nothing
-  , timestamp         = Nothing
-  , notificationSound = Nothing
-  }
+data CLI = CLI
+  { cliMessage :: Text
+  , cliTitle :: Text
+  , cliDevices :: [Text]
+  } deriving (Show, Eq)
+
+cli :: Parser CLI
+cli = CLI
+  <$> strOption
+      (  long "message"
+      <> metavar "TARGET"
+      <> help "Message string, takes in html" )
+  <*> strOption
+       (  long "title"
+       <> metavar "TARGET"
+       <> showDefault
+       <> value "pushover-cli"
+       <> help "Title of the message")
+  <*> many (strOption
+            (long "device"
+            <> metavar "TARGET"
+            <> help "devices to send to"))
 
 main :: IO ()
 main = do
-  r <- sendRequest testRequest
+  let opts = info (cli <**> helper)
+               ( fullDesc
+               <> progDesc "Send message to pushover"
+               <> header "pushove message from cli" )
 
+  apiToken <- readAPIKey "PS_API_TOKEN"
+  userKey <- readAPIKey "PS_USER_KEY"
+
+  (pSendMessage apiToken userKey) =<< execParser opts
+
+pSendMessage :: Text -> Text -> CLI -> IO ()
+pSendMessage api user opts = do
+  r <- sendRequest req
   putStrLn $ show $ status r
+  where
+    req = Request
+      { requestToken      = makeTokenOrError api
+      , requestUserKey    = makeTokenOrError user
+      , requestMessage    = text $ cliMessage opts
+      , devices           = cliDevices opts
+      , title             = Just (cliTitle opts)
+      , url               = Nothing
+      , priority          = Nothing
+      , timestamp         = Nothing
+      , notificationSound = Nothing
+      }
+
+readAPIKey :: String -> IO Text
+readAPIKey name = do
+  val <- lookupEnv name
+  case val of
+    Just v -> pure $ T.pack v
+    Nothing -> throwString ("env not set: " <> name)
